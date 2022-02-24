f0d918422127f92692186997f29bfdb0127f2169
Anthony Lower <anthonyjlower@gmail.com>
2022-02-23 19:53:34 -0600

    resize columns

--- app/javascript/components/TeamsTable.js
+++ app/javascript/components/TeamsTable.js
@@ -6,12 +6,26 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 const AgGrid = (rowData, columnDefs) => {
+  let gridApi = {}
+
+  const onGridReady = (params) => {
+    console.log(params.api)
+    gridApi = params.api
+  }
+
+  const sizeToFit = (gridApi) => {
+    console.log(gridApi)
+    gridApi.sizeColumnsToFit();
+  }
+
   return (
     <>
       <div className="ag-theme-alpine" style={{height: '90vh', width: '95vw'}}>
+        <button onClick={() => sizeToFit(gridApi)}>Size to Fit</button>
         <AgGridReact
           rowData={rowData}
           columnDefs={columnDefs}
+          onGridReady={onGridReady}
         >
         </AgGridReact>
       </div>
