d1d0feee5212fdd3722d92e57d338e0bc2b38c1d
Anthony Lower <anthonyjlower@gmail.com>
2022-02-23 20:47:54 -0600

    odds calculator

---
+++ app/services/odds_calculator.rb
@@ -0,0 +1,17 @@
+module OddsCalculator
+
+  class << self
+    # percent_success should be in XX.YY format
+    def calc_breakeven(percent_success)
+      percent_fail = 1.0 - percent_success
+      raw_odds = ((-0.05 + percent_fail) / percent_success) * 100
+      raw_odds >= 100 ? raw_odds : convert_negative(raw_odds)
+    end
+
+    private
+
+    def convert_negative(val)
+      -200 + val
+    end
+  end
+end
